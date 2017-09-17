0bf8022f769d9f3a8054a27eb4777621d494b1c7
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2017-09-17 11:21:02 -0600

    create handle_file functions

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -52,16 +52,22 @@ defmodule Servy.Handler do
     end
 
     def route(%{ method: "GET", path: "/about" } = conv) do
-        IO.puts __DIR__
-        file =
-            Path.expand("../../pages", __DIR__)
-            |> Path.join("about.html")
+        Path.expand("../../pages", __DIR__)
+        |> Path.join("about.html")
+        |> File.read
+        |> handle_file(conv)
+    end
 
-        case File.read(file) do
-            {:error, :enoent} -> %{ conv | status: 404, resp_body: "File not found" }
-            {:error, reason}  -> %{ conv | status: 500, resp_body: "File error: #{reason}" }
-            {:ok, content}    -> %{ conv | status: 200, resp_body: content }
-        end
+    def handle_file({:error, :enoent}, conv) do
+        %{ conv | status: 404, resp_body: "File not found" }
+    end
+
+    def handle_file({:error, reason}, conv) do
+        %{ conv | status: 500, resp_body: "File error: #{reason}" }
+    end
+
+    def handle_file({:ok, content}, conv) do
+        %{ conv | status: 200, resp_body: content }
     end
 
     def route(%{ path: path } = conv) do
