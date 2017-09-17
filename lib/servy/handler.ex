686cfcbc463d33b9ddbd3b404b7a648aec89c163
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2017-09-17 12:25:30 -0600

    preparing for post

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -56,23 +56,13 @@ defmodule Servy.Handler do
 
     def format_response(conv) do
         """
-        HTTP/1.0 #{conv.status} #{status_reason(conv.status)}
+        HTTP/1.0 #{Conv.full_status(conv)}
         Content-Type: text/html
         Content-Length: #{String.length(conv.resp_body)}
 
         #{conv.resp_body}
         """
     end
-
-    defp status_reason(code), do:
-        %{
-            200 => "OK",
-            201 => "Created",
-            401 => "Unauthorized",
-            403 => "Forbidden",
-            404 => "Not found",
-            500 => "Internal Server Error"
-        }[code]
 end
 
 
@@ -114,6 +104,17 @@ Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 
+"""
+
+request6 = """
+POST /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
+
+name=Baloo&type=Brown
 """
 
 # response = Servy.Handler.handle(request)
